46e34f91893fe7be3eadd5740d49b86bc7ee4aea
irg-ub-20-nvidia <hagenow@mit.edu>
2025-06-10 09:41:01 -0400

    adds resnet model to use in creating more structured model and schematic

--- nodes/visiondiffusiontesting/train_model.py
+++ nodes/visiondiffusiontesting/train_model.py
@@ -8,8 +8,8 @@ import time
 
 project_path = os.path.dirname(os.path.realpath(__file__))
 
-from vision_diffusion import VisionDiffusion
-from vision_diffusion import precompute_dino_feats
+from vision_diffusion import VisionDiffusion, precompute_dino_feats
+from vision_diffusion_resnet import VisionDiffusionResnet, precompute_resnet_feats
 
 
 def load_episode_dataset(dir_path: str = '.', pattern: str = 'episode_*.pkl'):
@@ -30,32 +30,109 @@ def load_episode_dataset(dir_path: str = '.', pattern: str = 'episode_*.pkl'):
     glob_path = os.path.join(dir_path, pattern)
 
     for fname in sorted(glob.glob(glob_path)):
+        print(fname)
         with open(fname, 'rb') as f:
             states_arr, actions_arr, images_arr = pickle.load(f)
         states_list.append(states_arr)
         actions_list.append(actions_arr)
         print(np.shape(images_arr))
         startt = time.time()
-        precompute_dino_feats(images_arr)
+        feats = precompute_resnet_feats(images_arr)
+        print("Feature shape",np.shape(feats))
         # print(time.time()-startt)
-        images_list.append(precompute_dino_feats(images_arr))
+        images_list.append(feats)
+
+    return states_list, actions_list, images_list
+
+def load_episode_dataset_dino(dir_path: str = '.', pattern: str = 'episode_*.pkl'):
+    """
+    Loads all pickle episodes in `dir_path` matching `pattern` and returns
+    three lists:
+      - states_list:  [ array of shape (n, T_i), ... ]
+      - actions_list: [ array of shape (m, T_i), ... ]
+      - images_list:  [ array of shape (T_i, H, W, C), ... ]
+    so you can call:
+        train(states_list, actions_list, images_list)
+    """
+    states_list  = []
+    actions_list = []
+    images_list  = []
+
+    # find and sort all matching files
+    glob_path = os.path.join(dir_path, pattern)
+
+    for fname in sorted(glob.glob(glob_path)):
+        print(fname)
+        with open(fname, 'rb') as f:
+            states_arr, actions_arr, images_arr = pickle.load(f)
+        states_list.append(states_arr)
+        actions_list.append(actions_arr)
+        print(np.shape(images_arr))
+        startt = time.time()
+        feats = precompute_dino_feats(images_arr)
+        print("Feature shape",np.shape(feats))
+        # print(time.time()-startt)
+        images_list.append(feats)
+
+    return states_list, actions_list, images_list
+
+def load_episode_dataset_no_encode(dir_path: str = '.', pattern: str = 'episode_*.pkl'):
+    """
+    Loads all pickle episodes in `dir_path` matching `pattern` and returns
+    three lists:
+      - states_list:  [ array of shape (n, T_i), ... ]
+      - actions_list: [ array of shape (m, T_i), ... ]
+      - images_list:  [ array of shape (T_i, H, W, C), ... ]
+    so you can call:
+        train(states_list, actions_list, images_list)
+    """
+    states_list  = []
+    actions_list = []
+    images_list  = []
+
+    # find and sort all matching files
+    glob_path = os.path.join(dir_path, pattern)
+    print("Loading data...")
+    for fname in sorted(glob.glob(glob_path)):
+        # print(fname)
+        with open(fname, 'rb') as f:
+            states_arr, actions_arr, images_arr = pickle.load(f)
+        states_list.append(states_arr)
+        actions_list.append(actions_arr)
+        images_list.append(images_arr)
+    
+    print(".... ",len(states_list)," files loaded")
 
     return states_list, actions_list, images_list
     
-
-
+    
 
 if __name__ == '__main__':
    
-    savelocation = project_path+'/visiondiffusion_ur5e.pkl'
+    # states, actions, images_resnet = load_episode_dataset(dir_path=".", pattern="tapping_6-6*.pkl")
 
-    policy = VisionDiffusion(num_epochs=1, pred_horizon=16)
-  
-    states, actions, images = load_episode_dataset(dir_path=".", pattern="episode_*.pkl")
+    # savelocation1 = project_path+'/visiondiffusionresnet_ur5e_6-9_obs_hor_2_pred_32.pkl'
 
-    print(np.shape(states))
-    policy.train(states, actions, images)
+    # policy = VisionDiffusionResnet(num_epochs=8000, obs_horizon=2, pred_horizon=32)
+    # policy.train(states, actions, images_resnet, savelocation1,save_epoch_freq=2000)
+    # policy.save(savelocation1)
+    # print("Finished saving ",policy.__class__.__name__," at",savelocation1)
 
+    # states, actions, images_dino = load_episode_dataset_dino(dir_path=".", pattern="tapping_6-6*.pkl")
+
+    # savelocation2 = project_path+'/visiondiffusion_ur5e_6-9_obs_hor_2_pred_32.pkl'
+
+    # policy = VisionDiffusion(num_epochs=8000, obs_horizon=2, pred_horizon=32)
+    # policy.train(states, actions, images_dino, savelocation2,save_epoch_freq=2000)
+    # policy.save(savelocation2)
+    # print("Finished saving ",policy.__class__.__name__," at",savelocation2)
+
+        # states, actions, images_resnet = load_episode_dataset(dir_path=".", pattern="tapping_6-6*.pkl")
+
+    # after bringing the encoding back in!!!
+    savelocation = project_path+'/visiondiffusionresnet_distory_ur5e_6-10_obs_hor_2_pred_32.pkl'
+    states, actions, images = load_episode_dataset_no_encode(dir_path=".", pattern="tapping_6-6*.pkl")
+    policy = VisionDiffusionResnet(num_epochs=8000, obs_horizon=2, pred_horizon=32)
+    policy.train(states, actions, images, savelocation,save_epoch_freq=1000,distort=True)
     policy.save(savelocation)
-
     print("Finished saving ",policy.__class__.__name__," at",savelocation)
---
+++ nodes/visiondiffusiontesting/visualize_dataset.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python3
+import glob
+import pickle
+import numpy as np
+import plotly.graph_objs as go
+import argparse
+import os
+
+def load_trajectories(pattern):
+    trajs = []
+    total_points = 0
+
+    for fname in sorted(glob.glob(pattern)):
+        with open(fname, 'rb') as f:
+            states, actions, images = pickle.load(f)
+            data = np.array(states)
+            if data.shape[0] < 3:
+                raise ValueError(f"{fname} has fewer than 3 state dimensions")
+            xyz = data[:3, :].T  # shape: (T, 3)
+            trajs.append((os.path.basename(fname), xyz))
+            total_points += xyz.shape[0]
+
+    print(f"Loaded {len(trajs)} episodes with {total_points} total datapoints.")
+    return trajs
+
+def equal_axis_range(fig):
+    x_vals, y_vals, z_vals = [], [], []
+    for trace in fig.data:
+        x_vals.extend(trace.x)
+        y_vals.extend(trace.y)
+        z_vals.extend(trace.z)
+
+    x_vals = np.array(x_vals)
+    y_vals = np.array(y_vals)
+    z_vals = np.array(z_vals)
+
+    x_range = [np.min(x_vals), np.max(x_vals)]
+    y_range = [np.min(y_vals), np.max(y_vals)]
+    z_range = [np.min(z_vals), np.max(z_vals)]
+
+    max_range = max(
+        x_range[1] - x_range[0],
+        y_range[1] - y_range[0],
+        z_range[1] - z_range[0]
+    )
+
+    def center_range(r):
+        mid = sum(r) / 2
+        return [mid - max_range/2, mid + max_range/2]
+
+    return dict(
+        xaxis=dict(range=center_range(x_range)),
+        yaxis=dict(range=center_range(y_range)),
+        zaxis=dict(range=center_range(z_range)),
+    )
+
+def plot_3d_trajectories(trajs):
+    fig = go.Figure()
+
+    for name, data in trajs:
+        fig.add_trace(go.Scatter3d(
+            x=data[:, 0],
+            y=data[:, 1],
+            z=data[:, 2],
+            mode='lines+markers',
+            name=name,
+            marker=dict(size=2),  # 👈 smaller marker size
+            line=dict(width=2)    # optional: keep lines visible
+        ))
+    fig.update_layout(
+        scene=equal_axis_range(fig),
+        scene_aspectmode='cube',
+        title="3D Trajectories",
+        margin=dict(l=0, r=0, b=0, t=30),
+        legend=dict(x=0.01, y=0.99)
+    )
+    fig.show()
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--pattern', type=str, default='tapping*.pkl',
+                        help='Glob pattern for input .pkl files (default: "episode_*.pkl")')
+    args = parser.parse_args()
+
+    trajectories = load_trajectories(args.pattern)
+    plot_3d_trajectories(trajectories)
